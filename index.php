9dcd7cb27deb1e088eb1709a11c8c84098d39684
khaerulanam250 <muhammadkhaerulanam45@gmail.com>
2024-07-23 13:26:38 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,155 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Website pendaftaran peserta didik baru secara online SMP SWADHIPA 1 NATAR">
+    <meta name="author" content="Theokusumaaa">
+    <title>MTSS BAITURRAHIM KABAR</title>
+    <!-- <link rel="canonical" href="nama-domain"> -->
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="assets/img/logo.png" sizes="180x180">
+    <link rel="icon" href="assets/img/swd.png" sizes="32x32" type="image/png">
+
+    <!-- <meta name="theme-color" content="#563d7c"> -->
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+            font-size: 3.5rem;
+            }
+        }
+        .nav-link {
+            padding-left: 30px !important;
+        }
+    </style>
+    <!-- Custom styles for this template -->
+<body>
+    <header>
+        <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light shadow" style="background: white !important; height: 70px;">
+            <div class="container">
+                <a class="navbar-brand" href="#"><b>MTSS BAITURRAHIM KABAR</b></a>
+                
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav ml-auto">
+                        <!-- <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li> -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">TENTANG KAMI</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">JADWAL</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="login.php">MASUK</a>
+                        </li>
+                    </ul>
+                    <a href="registrasi.php" class="btn btn-danger rounded-pill ml-3">DAFTAR</a>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main role="main" style="position: relative">
+        <div class="container mt-5">
+            <div class="row pt-5">
+                <div class="col-md-7">
+                    <h2 class="text-dark">PENDAFTARAN ONLINE <br> MTSS BAITURRAHIM KABAR <br></h2>
+                    <p class="lead">
+                        Untuk calon pendaftar siswa baru bisa mendaftar melalui website ini atau langsung datang ke tempat pendaftaran
+                    </p>
+                    <button class="btn btn-danger rounded-pill">DAFTAR SEKARANG</button>
+                </div>
+                <div class="col-md-5">
+                    <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="300" xmlns="http://www.w3.org/2000/svg" preserveaspectratio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x300</text></svg>
+                </div>
+            </div>
+
+            <div class="row mt-5">
+                <div class="col-lg-4">
+                    <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveaspectratio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+                    <h2>Heading</h2>
+                    <p>
+                       Isi Konten
+                    </p>
+                    <p>
+                        <a class="btn btn-secondary" href="#" role="button">View details &raquo;</a>
+                    </p>
+                </div>
+                <!-- /.col-lg-4 -->
+                <div class="col-lg-4">
+                    <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveaspectratio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+                    <h2>Heading</h2>
+                    <p>
+                    Isi Konten
+                    </p>
+                    <p>
+                        <a class="btn btn-secondary" href="#" role="button">View details &raquo;</a>
+                    </p>
+                </div>
+                <!-- /.col-lg-4 -->
+                <div class="col-lg-4">
+                    <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveaspectratio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+                    <h2>Heading</h2>
+                    <p>
+                    Isi Konten
+                    </p>
+                    <p>
+                        <a class="btn btn-secondary" href="#" role="button">View details &raquo;</a>
+                    </p>
+                </div>
+                <!-- /.col-lg-4 -->
+            </div>
+            <!-- /.row -->
+            <!-- START THE FEATURETTES -->
+            <hr class="featurette-divider">
+            <div class="row featurette">
+
+                <div class="col-md-7 order-md-2">
+                    <h2 class="featurette-heading">Judul <span class="text-muted"></span></h2>
+                    <p class="lead">
+                    Isi Konten
+                    </p>
+                </div>
+                <div class="col-md-5 order-md-1">
+                    <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveaspectratio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+                </div>
+            </div>
+            <hr class="featurette-divider">
+            <div class="row featurette">
+                <div class="col-md-7">
+                    <h2 class="featurette-heading">Judul <span class="text-muted"></span></h2>
+                    <p class="lead">
+                    Isi Konten
+                    </p>
+                </div>
+                <div class="col-md-5">
+                    <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveaspectratio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+                </div>
+            </div>
+            <hr class="featurette-divider">
+            <!-- /END THE FEATURETTES -->
+        </div>
+        <!-- /.container -->
+    </main>
+    <!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="/docs/4.4/assets/js/vendor/jquery.slim.min.js"><\/script>')</script>
+    <script src="/docs/4.4/dist/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script> -->
+
+</body>
+</html>
